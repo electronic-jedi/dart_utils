b7d718996ce302ee1c0ef5d2ce734af72f6920e3
dalitso <dalitso.1sa@gmail.com>
2022-03-12 14:34:16 +0200

    added `[DateTime.elapsedDuration_]`

--- lib/time.dart
+++ lib/time.dart
@@ -6,6 +6,13 @@
 /// A library to work with time
 library time;
 
+String _plural(int quantity, [String plural = 's', String singular = '', int threshold = 1]) {
+  if (quantity > threshold)
+    return plural;
+  else
+    return singular;
+}
+
 /// Extension utilities on the [Duration] class
 extension DurationUtils on Duration {
   /// Attempts to get the duration in years
@@ -51,7 +58,11 @@ extension DateUtils on DateTime {
   }
 
   /// Get the time in the format `1:04 PM`.
-  String get meridianTime {
+  @Deprecated('use "meridianTime_"')
+  String get meridianTime => meridianTime_;
+
+  /// Get the time in the format `1:04 PM`.
+  String get meridianTime_ {
     final suffix = hour >= 12 ? 'PM' : 'AM';
     final _hour = hour > 12 ? hour - 12 : hour;
     final _minute = minute < 10 ? '0$minute' : minute;
@@ -59,13 +70,38 @@ extension DateUtils on DateTime {
   }
 
   /// Get the time in the format `13:04`.
-  String get timeOfDay {
+  @Deprecated('use "timeOfDay_"')
+  String get timeOfDay => timeOfDay_;
+
+  /// Get the time in the format `13:04`.
+  String get timeOfDay_ {
     final _minute = minute < 10 ? '0$minute' : minute;
     return '$hour:$_minute';
   }
 
+  String get elapsedDuration_ {
+    String elapsed;
+    String suffix;
+
+    if (year >= 1)
+      suffix = 'year' + _plural(year);
+    else if (month >= 1)
+      suffix = 'month' + _plural(month);
+    else if (day >= 1)
+      suffix = 'day' + _plural(day);
+    else if (minute >= 1)
+      suffix = 'minute' + _plural(minute);
+    else
+      suffix = 'second' + _plural(second);
+    return elapsed;
+  }
+
   /// Attempts to get the full name of the month.
-  String get monthName {
+  @Deprecated('use "monthName_"')
+  String get monthName => monthName_;
+
+  /// Attempts to get the full name of the month.
+  String get monthName_ {
     String name;
     switch (month) {
       case 1:
